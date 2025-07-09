4bc84711036a6da375f1ebe7dfc34fab80cf0e2d
gitji270 <akshun.jindal.cse.2022@gmail.com>
2025-07-09 13:04:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+ function init() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+init()
+ 
+ 
+// Animate navigation items
+ gsap.from("#nav h1", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  delay: 0.2,
+  stagger: 0.1,
+  ease: "power2.out"
+});
+ 
